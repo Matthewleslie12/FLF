c5e1c1e875d619fe2fb332e42c63a63c3c2d0516
Matthew Leslie <matthewleslie155@gmail.com>
2023-06-06 12:02:04 -0400

    added animation data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,13 @@
+import Lottie from "lottie-react";
+import animationData from "../assets/animations/126196-jansmavip-html-and-coding-pages.json";
+
 const HomePage = () => {
-  return <div>gang</div>;
+  return (
+    <div>
+      gang
+      <Lottie animationData={animationData} loop={true} />
+    </div>
+  );
 };
 
 export default HomePage;
