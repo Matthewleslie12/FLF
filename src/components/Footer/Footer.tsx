e0be3d2d47b81348ccf642919a01b7fc08c0c104
BenjaminStubina <benjamin.stubina@gmail.com>
2023-06-19 19:45:49 -0400

    Create mobile footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import react from '../../assets/Tech-icons/react.png';
+import tailwind from '../../assets/Tech-icons/tailwind-css.svg';
+import typescript from '../../assets/Tech-icons/Typescript_logo_2020.svg.png';
+import post from '../../assets/Tech-icons/Postgresql_elephant.svg.png';
+
+const Footer = () => {
+    return (
+        <section className="flex-col footer-gradient-2 text-xs px-[1.5rem] py-[1.5rem] ">
+            <h1 className="text-white font-bebas">
+                THANKS FOR VISITING
+            </h1>
+            <div className="flex justify-between ">
+                <div className="w-[50%] border-solid border-2 border-white h-[80px] flex justify-center items-center">
+                    {/* <PingPong /> */}
+                </div>
+                <div className="w-[50%] border-solid border-2 border-white h-[80px] ">
+                    <div className="flex-col">
+                        <span className="flex text-[12px] text-white leading-[40px] align-middle justify-center font-oxanium">
+                            Website Designed with
+                        </span>
+                        <div className="flex flex-row justify-between">
+                            <img src={react} alt="React Logo" className="w-[25%] aspect-[4/3] object-contain" />
+                            <img src={tailwind} alt="React Logo" className="w-[25%] aspect-[4/3] object-contain" />
+                            <img src={typescript} alt="React Logo" className="w-[25%] aspect-[4/3] object-contain" />
+                            <img src={post} alt="React Logo" className="w-[25%] aspect-[4/3] object-contain" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Footer;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/Nav/NavBar";
 import WorkWithUs from "../components/WorkWithUs.tsx/WorkWithUs";
+import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
   return (
     <div className="">
       <NavBar />
       <WorkWithUs />
+      <Footer />
     </div>
   );
 };
