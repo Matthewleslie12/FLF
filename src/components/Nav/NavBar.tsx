c0e91cfab3d05160b084dcede60202efccee3820
ngkao <ng.kao.ca@gmail.com>
2023-07-04 22:10:05 -0400

    add smooth scroll down and up upon nav click

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -2,6 +2,7 @@ import logo from "../../assets/Logo.png";
 import line from "../../assets/nav-icons/Line 2.svg";
 import useMenuState from "../../hooks/navHooks.tsx";
 import CloseButton from "../../assets/nav-icons/close-btn.svg";
+import DevilLogo from "../../assets/nav-icons/devil-logo.svg"
 
 const NavBar = () => {
 
@@ -12,12 +13,25 @@ const NavBar = () => {
     console.log(isMenuOpen, "isMenuOpen")
   };
 
+  const smoothScrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  // Need to add id to each section. Ex id="team" id="projects" id="home"
+
   return (
-    <nav className="flex justify-between pt-4 h-20">
-      <img src={logo} alt="FLF Logo" className="h-5 w-40" />
+    <nav className="flex justify-between items-center h-20 mx-[1rem]">
+      {!isMenuOpen ? 
+        <img src={logo} alt="Full FLF Logo" className="h-6 w-auto" />
+        :
+        <img src={DevilLogo} alt="Devil FLF Logo"/>
+      }
       {!isMenuOpen ? 
             <div
-            className="rounded-full w-12 h-12 hamburger-gradient mr-4 flex flex-col py-2 cursor-pointer justify-center hover:border-2 hover:border-white-500"
+            className="rounded-full w-12 h-12 hamburger-gradient flex flex-col py-2 cursor-pointer justify-center hover:border-2 hover:border-white-500"
             onClick={toggleMenu} 
           >
             <img src={line} alt="" className="mx-auto py-1" />
@@ -30,16 +44,23 @@ const NavBar = () => {
           >
             <div className="flex flex gap-4 align-item">
               <ul className="flex gap-4">
-                <li>HOME</li>
-                <li>PROJECTS</li>
-                <li>TEAM</li>
+                <li 
+                  className="cursor-pointer"
+                  onClick={() => smoothScrollToSection('home')}
+                >HOME</li>
+                <li 
+                  className="cursor-pointer"
+                  onClick={() => smoothScrollToSection('projects')}
+                >PROJECTS</li>
+                <li 
+                  className="cursor-pointer"
+                  onClick={() => smoothScrollToSection('team')}
+                >TEAM</li>
               </ul>
-              <img className="pb-1" onClick={toggleMenu} src={CloseButton} alt="Close Button" />
+              <img className="pb-1 cursor-pointer" onClick={toggleMenu} src={CloseButton} alt="Close Button" />
             </div>
           </div>
-      
       }
-
     </nav>
   );
 };
