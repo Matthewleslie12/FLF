93273d9ea233a4597d21efd59d3fef19425140f9
BenjaminStubina <benjamin.stubina@gmail.com>
2023-07-11 00:27:07 -0400

    Update pong animation at breakpoints

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,27 +7,29 @@ import penguin from '../../assets/Tech-icons/oie_4t1L8VhfL5Es.png'
 
 const Footer = () => {
     return (
-        <section className="flex flex-col footer-gradient-2 text-xs px-[1.5rem] md:px-0 py-[1.5rem]">
-            <div className='flex justify-between align-middle w-[312px] mx-auto'>
-                <h1 className="flex text-white font-bebas items-end">
-                    THANKS FOR VISITING
-                </h1>
-                <img src={penguin} alt="" className=""></img>
-            </div>
-            <div className="flex flex-col space-y-2 w-[312px] md:flex-row md:w-[624px] md:mx-auto md:justify-between mx-auto">
-                <div className="flex w-[312px] h-[80px] mt-5 align-middle">
-                    <PingPong />
+        <section className=" text-xs px-[1.5rem] md:px-[50px] footer-gradient-2 py-[3rem] mt-[1.5rem] md:mt-[3rem] xl:px-0">
+            <div className='flex flex-col xl:max-w-[1280px] xl:mx-auto xl:px-[68px]'>
+                <div className='flex justify-between md:justify-normal align-middle w-[312px] md:w-[100%] mx-auto'>
+                    <h1 className="flex text-white font-bebas items-end md:text-[40px] xl:text-[50px]">
+                        THANKS FOR VISITING
+                    </h1>
+                    <img src={penguin} alt="" className="absolute left-[calc(50%+3.75rem)] inset-y-[38rem] md:inset-x-[22rem] md:inset-y-[39rem] md:w-[150px] xl:inset-y-[46rem] xl:left-[calc(50%-13rem)] xl:w-[225px]"></img>
                 </div>
-                <div className="flex md:w-[312px] h-[80px]">
-                    <div className="flex flex-col">
-                        <span className="flex text-[12px] text-white leading-[40px] align-middle justify-center font-oxanium">
-                            Website Designed with
-                        </span>
-                        <div className="flex flex-row justify-between w-[312px] mx-auto">
-                            <img src={react} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
-                            <img src={tailwind} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
-                            <img src={typescript} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
-                            <img src={post} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
+                <div className="flex flex-col space-y-2 w-[312px] md:flex-row md:w-[100%] md:mx-auto md:justify-between mx-auto">
+                    <div className="flex w-[312px] xl:w-[750px] h-[80px] xl:h-[200px] mt-5 align-middle">
+                        <PingPong />
+                    </div>
+                    <div className="flex md:w-[312px] h-[80px] xl:h-[200px]">
+                        <div className="flex flex-col justify-end md:space-y-3">
+                            <span className="flex text-[12px] text-white leading-[40px] md:text-[20px] align-middle justify-center font-oxanium">
+                                Website Designed with
+                            </span>
+                            <div className="flex flex-row justify-between w-[312px] mx-auto">
+                                <img src={react} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
+                                <img src={tailwind} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
+                                <img src={typescript} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
+                                <img src={post} alt="React Logo" className="w-[50px] aspect-[4/3] object-contain" />
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/PingPong/PingPong.tsx
+++ src/components/PingPong/PingPong.tsx
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom'
 
 const PingPong = () => {
     return (
-        <div className="field">
+        <Link className='field' to='https://www.linkedin.com/company/freelance-fiends-flf/' target='_blank' rel='noreferrer'>
             <div className="ping"></div>
             <div className="pong"></div>
-            <Link to='https://www.linkedin.com/company/freelance-fiends-flf/' target='_blank' rel='noreferrer'>
-                <img src={ball} alt="" className='ball md:ballTablet absolute'></img>
-            </Link>
-        </div>
+            <img src={ball} alt="" className='ball absolute'></img>
+        </Link>
     )
 }
 
