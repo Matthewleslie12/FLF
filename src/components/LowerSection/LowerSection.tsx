5fc24488cf507ff3a58a9a2f0fe147922dd16ee8
BenjaminStubina <benjamin.stubina@gmail.com>
2023-07-21 08:45:53 -0400

    Fix Contact Form behaviour in Tablet view

--- src/components/LowerSection/LowerSection.tsx
+++ src/components/LowerSection/LowerSection.tsx
@@ -5,7 +5,7 @@ import WorkWithUs from "../WorkWithUs/WorkWithUs";
 const LowerSection = () => {
     return (
         <section className="flex-col relative overflow-hidden">
-            <div className="flex flex-col md:flex md:flex-row md:h-[637px] xl:h-[813px] md:justify-between xl:max-w-[1280px] xl:mx-auto">
+            <div className="flex flex-col md:flex md:flex-row md:h-[637px] xl:h-[813px] xl:justify-between xl:max-w-[1280px] xl:mx-auto">
                 <WorkWithUs />
                 <ContactForm />
             </div>
